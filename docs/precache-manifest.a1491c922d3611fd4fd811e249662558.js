ea60b9110a36040c85ec4901ccfcb22a15f189ee
Ricardo José soto sáez <ricardosotosaez@hotmail.com>
2020-09-04 11:12:44 -0400

    docs agregado

---
+++ docs/precache-manifest.a1491c922d3611fd4fd811e249662558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8f13574c946a68b072724f842b70c73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c857716f667340571650",
+    "url": "/static/css/main.ac22ac38.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c857716f667340571650",
+    "url": "/static/js/main.fb567bf6.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
